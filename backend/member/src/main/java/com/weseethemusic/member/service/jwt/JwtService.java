d9fe0d0ebbf6cea5b49e7ae7b62652ca53af98a0
서장원 <seojang980510@naver.com>
2024-10-29 16:25:54 +0900

    [BE]: 로그인/아웃/회원가입 등등 서비스 만들기 - 토큰 재발급  (S11P31D203-129)

--- backend/member/src/main/java/com/weseethemusic/member/service/jwt/JwtService.java
+++ backend/member/src/main/java/com/weseethemusic/member/service/jwt/JwtService.java
@@ -1,5 +1,6 @@
-package com.weseethemusic.member.service;
+package com.weseethemusic.member.service.jwt;
 
 public interface JwtService {
 
+    String reIssueRefreshToken(String token, String refreshToken);
 }
