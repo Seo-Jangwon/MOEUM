96993e63613f0a8fe2540f8b994df68060ea2e66
김시현 <gus3260@naver.com>
2024-10-31 18:06:19 +0900

    Feat/chore/main page
    Former-commit-id: fb202d1e952dfd69c6b0cf64542e1ca96dc99ce7

---
+++ frontend/src/pages/MainPage/NewList/NewList.tsx
@@ -0,0 +1,88 @@
+import lala from '../image/lala.jpg'
+
+
+interface Music {
+  title: string;
+  img: string;
+  artist: string;
+}
+
+const mokData: { music: Music[] } = {
+  music: [
+    {
+      title: 'apt',
+      img: 'apt.jpg',
+      artist: 'rose',
+    },
+    {
+      title: 'apt',
+      img: 'apt.jpg',
+      artist: 'rose',
+    },
+    {
+      title: 'apt',
+      img: 'apt.jpg',
+      artist: 'rose',
+    },
+    {
+      title: 'apt',
+      img: 'apt.jpg',
+      artist: 'rose',
+    },
+    {
+      title: 'apt',
+      img: 'apt.jpg',
+      artist: 'rose',
+    },
+    {
+      title: 'apt',
+      img: 'apt.jpg',
+      artist: 'rose',
+    },
+    {
+      title: 'apt',
+      img: 'apt.jpg',
+      artist: 'rose',
+    },
+    {
+      title: 'apt',
+      img: 'apt.jpg',
+      artist: 'rose',
+    },
+    {
+      title: 'apt',
+      img: 'apt.jpg',
+      artist: 'rose',
+    },
+    {
+      title: 'apt',
+      img: 'apt.jpg',
+      artist: 'rose',
+    },
+    {
+      title: 'apt',
+      img: 'apt.jpg',
+      artist: 'rose',
+    },
+    {
+      title: 'apt',
+      img: 'apt.jpg',
+      artist: 'rose',
+    },
+    {
+      title: 'apt',
+      img: 'apt.jpg',
+      artist: 'rose',
+    },
+  ],
+};
+
+
+
+const NewList = () => {
+  return (
+    <div>NewList</div>
+  )
+}
+
+export default NewList
