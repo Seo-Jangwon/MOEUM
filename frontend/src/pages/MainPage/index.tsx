f687d2adddda6192d3f52d941369460482baec8b
김시현 <gus3260@naver.com>
2024-11-01 01:18:25 +0900

    feat: 새로운 메인페이지 생성

--- frontend/src/pages/MainPage/index.tsx
+++ frontend/src/pages/MainPage/index.tsx
@@ -1,34 +1,27 @@
-import { css } from '@emotion/react';
-import GenreList from './GenreList/GenreLIst';
+import GenreList from './GenreList/GenreList';
 import NewList from './NewList/NewList';
 import PopularList from './PopularList/PopularList';
 import PopularPlayList from './PopularPlayList/PopularPlayList';
+import { s_box, s_container } from './style';
 
 const MainPage = () => {
   return (
     // 전체 레이아웃
-    <div
-      css={css`
-        display: flex;
-        width: 75%;
-        flex-direction: column;
-        align-self: center;
-      `}
-    >
+    <div css={s_container}>
       {/* 신곡 리스트 */}
-      <div>
+      <div css={s_box}>
         <NewList />
       </div>
       {/* 인기 리스트 */}
-      <div>
+      <div css={s_box}>
         <PopularList />
       </div>
       {/* 플레이 리스트 */}
-      <div>
+      <div css={s_box}>
         <PopularPlayList />
       </div>
       {/* 장르 리스트 */}
-      <div>
+      <div css={s_box}>
         <GenreList />
       </div>
     </div>
