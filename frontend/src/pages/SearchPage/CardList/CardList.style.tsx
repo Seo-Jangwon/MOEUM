44def71bb042f210f61690a5c2cb46bb382cd9cb
kayas <wogur1833@naver.com>
2024-11-01 14:42:19 +0900

    feat : 검색 결과 페이지 구현

---
+++ frontend/src/pages/SearchPage/CardList/CardList.style.tsx
@@ -0,0 +1,51 @@
+import { css } from '@emotion/react';
+
+export const s_container = css`
+  display: flex;
+  flex-direction: column;
+  word-break: break-all;
+  @media (max-width: 768px) {
+    width: 90%;
+  }
+  @media (min-width: 768px) {
+    width: 60%;
+  }
+`;
+
+export const s_titleContainer = css`
+  display: flex;
+  justify-content: space-between;
+  font-size: 20px;
+  text-align: center;
+  margin-bottom: 20px;
+  @media (max-width: 768px) {
+    padding: 0 10%;
+  }
+`;
+export const s_elementsContainer = css`
+  display: grid;
+  width: 100%;
+  @media (max-width: 768px) {
+    grid-template-rows: 1fr 1fr;
+    grid-template-columns: 1fr 1fr;
+  }
+  @media (min-width: 768px) {
+    grid-template-rows: 1fr;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
+  row-gap: 25px;
+`;
+
+export const s_elementContainer = css`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const s_elementImage = (isBorder: boolean) => css`
+  width: 80%;
+  aspect-ratio: 1/1;
+  border-radius: ${isBorder ? '100%' : '10px'};
+`;
--- frontend/src/pages/SearchPage/MusicList/MusicList.style.ts
+++ frontend/src/pages/SearchPage/MusicList/MusicList.style.ts
@@ -1,10 +1,9 @@
-import { css, Theme } from '@emotion/react';
+import { css } from '@emotion/react';
 
-export const s_container = (theme: Theme) => css`
-  color: ${theme.colors.white};
+export const s_container = css`
   display: flex;
   flex-direction: column;
-  padding-bottom: 20px;
+  word-break: break-all;
   @media (max-width: 768px) {
     width: 90%;
   }
@@ -18,51 +17,80 @@ export const s_titleContainer = css`
   justify-content: space-between;
   font-size: 20px;
   text-align: center;
+  @media (max-width: 768px) {
+    padding: 0 10%;
+  }
 `;
 
 export const s_elementsContainer = css`
   display: flex;
   width: 100%;
-  aspect-ratio: 1/2;
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 
 export const s_firstElement = css`
-  width: 40%;
   display: flex;
   flex-direction: column;
   aspect-ratio: 1/1;
+  margin: 5px 0;
   @media (max-width: 768px) {
-    width: 80%;
+    width: 90%;
+    justify-content: center;
+    align-items: center;
+  }
+  @media (min-width: 768px) {
+    width: 40%;
+  }
+`;
+
+export const s_firstElementText = css`
+  @media (max-width: 768px) {
+    padding-top: 5px;
+    width: 90%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
   }
 `;
 export const s_firstElementImage = css`
-  width: 80%;
+  width: 90%;
   aspect-ratio: 1/1;
   border-radius: 20px;
 `;
 
 export const s_otherElementContainer = css`
   width: 60%;
-  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  aspect-ratio: 3/2;
+  row-gap: 5px;
+  @media (max-width: 768px) {
+    width: 85%;
+  }
 `;
 
 export const s_otherElement = css`
   width: 100%;
+  aspect-ratio: 6/1;
   justify-content: space-between;
   display: flex;
-  height: 25%;
+  cursor: pointer;
 `;
 
 export const s_otherElementLeftChild = css`
   display: flex;
   align-items: center;
-  height: 100%;
-  aspect-ratio: 1/1;
   text-align: center;
+  width: 50%;
 `;
 
 export const s_otherElementImage = css`
-  height: 25%;
+  width: 35%;
+  aspect-ratio: 1/1;
   margin-right: 5px;
   border-radius: 10px;
 `;
