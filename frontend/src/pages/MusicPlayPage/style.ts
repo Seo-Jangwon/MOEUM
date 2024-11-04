2c50513524892bcff64c9afcaaee4fa1161211a7
kayas <wogur1833@naver.com>
2024-11-04 16:32:59 +0900

    style: 폰트 색깔 변경
    Former-commit-id: 631382341f8743dded1a7c9f0840075b76cf1c57

--- frontend/src/pages/MusicPlayPage/style.ts
+++ frontend/src/pages/MusicPlayPage/style.ts
@@ -1,6 +1,7 @@
-import { css } from '@emotion/react';
+import { css, Theme } from '@emotion/react';
 
-export const s_container = css`
+export const s_container = (theme: Theme) => css`
+  color: ${theme.colors.white};
   @media (max-width: 768px) {
     display: flex;
     justify-content: center;
