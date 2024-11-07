1d0b04af9d4cdd6356db59064a658aadfd7475b4
최진오 <sidamo.dev@gmail.com>
2024-11-07 10:32:57 +0900

    chore: monorepo로 전환 (S11P31D203-93)
    Former-commit-id: 446b87023be3c99637a9111f3e80e97861fce045

--- backend/member/src/main/java/com/weseethemusic/member/common/constants/SecurityConstants.java
+++
@@ -1,9 +0,0 @@
-package com.weseethemusic.member.common.constants;
-
-public class SecurityConstants {
-
-    public static final String JWT_HEADER = "Authorization";
-    public static final String REFRESH_TOKEN_HEADER = "Refresh-Token";
-    public static final String REFRESH_TOKEN_COOKIE = "refresh_token";  // 추가
-
-}
