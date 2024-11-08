ae923a8af27e9a5f49f789f97dd2d71780596120
서장원 <seojang980510@naver.com>
2024-11-08 10:57:53 +0900

    [be] fix: yml수정, recommendation service 추가  (S11P31D203-203)
    Former-commit-id: d6ed0da0deb550a4999f32d9278561cf268a5550

---
+++ backend/recommendation/src/main/java/com/weseethemusic/recommendation/common/entity/Music.java
@@ -0,0 +1,46 @@
+package com.weseethemusic.recommendation.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Music {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "genre_id", nullable = false)
+    private Genre genre;
+
+    @Column(nullable = false)
+    private int duration;
+
+    private double danceability;
+    private double loudness;
+    private boolean mode;
+    private double speechiness;
+    private double acousticness;
+    private double valence;
+    private double tempo;
+    private double energy;
+
+}
