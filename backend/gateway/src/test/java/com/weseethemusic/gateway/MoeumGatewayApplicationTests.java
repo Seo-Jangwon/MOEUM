bd1d11fdbbc0247a156344a6acddc655ebc693a9
민서령 <msr980929@gmail.com>
2024-11-08 09:42:11 +0900

    [BE] chore: gateway/develop 병합 (S11P31D203-195)
    Former-commit-id: 51c26abd77de07aaeccd1856a6bba5e7dab4aa5a

---
+++ backend/gateway/src/test/java/com/weseethemusic/gateway/MoeumGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weseethemusic.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoeumGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
