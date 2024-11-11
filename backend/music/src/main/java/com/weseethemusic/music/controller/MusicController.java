16a392b00c7308557fa73a1463ccb3ed53cec475
민서령 <msr980929@gmail.com>
2024-11-11 16:17:36 +0900

    [BE] feat: 아티스트 전체 디스코그래피 조회 API 구현 (S11P31D203-236)
    Former-commit-id: eb702b5241e043316363b12463b1f4430fc35ce0

--- backend/music/src/main/java/com/weseethemusic/music/controller/MusicController.java
+++ backend/music/src/main/java/com/weseethemusic/music/controller/MusicController.java
@@ -3,11 +3,14 @@ package com.weseethemusic.music.controller;
 import com.weseethemusic.music.common.exception.CustomException;
 import com.weseethemusic.music.common.exception.ErrorCode;
 import com.weseethemusic.music.dto.ResponseDto;
+import com.weseethemusic.music.dto.general.GeneralDiscographyDto;
 import com.weseethemusic.music.dto.general.GeneralMusicDto;
 import com.weseethemusic.music.service.MusicServiceImpl;
 import java.util.List;
+import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,6 +47,22 @@ public class MusicController {
         }
 
         return ResponseDto.res(200, latestMusicDtos);
+    }
+
+    // 아티스트 전체 디스코그래피 조회
+    @GetMapping("/artist/{artistId}/discography")
+    public ResponseDto<List<GeneralDiscographyDto>> getAllDiscography(@PathVariable long artistId) {
+        List<GeneralDiscographyDto> result;
+
+        try {
+            result = musicService.getAllDiscography(artistId);
+        } catch (NoSuchElementException e) {
+            throw new CustomException(ErrorCode.NOT_FOUND, "아티스트 정보를 찾을 수 없습니다.");
+        } catch (Exception e) {
+            throw new CustomException(ErrorCode.INTERNAL_SERVER_ERROR, "내부 서버 오류");
+        }
+
+        return ResponseDto.res(200, result);
     }
 
 }
