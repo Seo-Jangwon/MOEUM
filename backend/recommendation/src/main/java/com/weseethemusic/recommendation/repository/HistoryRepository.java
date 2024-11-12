1de7cb522347bc82a2bba799c3503f59aed03718
민서령 <msr980929@gmail.com>
2024-11-12 21:41:01 +0900

    fix: Revert files to old version(ccabdc16177254dfae50c99456270b10c981d9dd [formerly 08b027ff9c85afeefd5230ee70e720caf8833155]) to fix 502 error
    Former-commit-id: 93aaec5edde8d370c8b2861b41560dba7ac11595

--- backend/recommendation/src/main/java/com/weseethemusic/recommendation/repository/HistoryRepository.java
+++ backend/recommendation/src/main/java/com/weseethemusic/recommendation/repository/HistoryRepository.java
@@ -6,7 +6,6 @@ import com.weseethemusic.recommendation.common.entity.Music;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -16,11 +15,5 @@ public interface HistoryRepository extends JpaRepository<History, HistoryId> {
     List<Music> findAllByMemberId(Long memberId);
 
     void deleteByMemberIdAndMusic_Id(Long memberId, Long musicId);
-
-    @Query("SELECT h FROM History h JOIN FETCH h.music m " +
-        "LEFT JOIN FETCH m.artistMusics am " +
-        "LEFT JOIN FETCH am.artist " +
-        "LEFT JOIN FETCH m.genre " +
-        "WHERE h.memberId = :memberId")
-    List<History> findByMemberId(@Param("memberId") long memberId);
+    
 }
