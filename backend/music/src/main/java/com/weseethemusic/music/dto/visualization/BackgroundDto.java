a98570e883225b923635c5d4d1c990c1adf19d49
민서령 <msr980929@gmail.com>
2024-11-18 12:32:30 +0900

    [BE] fix: 음악 시각화 데이터 API를 S3와 연결
    Former-commit-id: 2f43372fc51f03041db1146d770d9eb83a4cdc25

---
+++ backend/music/src/main/java/com/weseethemusic/music/dto/visualization/BackgroundDto.java
@@ -0,0 +1,18 @@
+package com.weseethemusic.music.dto.visualization;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BackgroundDto {
+
+    private double start;
+    private double end;
+    private String color;
+
+}
--- backend/music/src/main/java/com/weseethemusic/music/service/MusicVisualizationServiceImpl.java
+++ backend/music/src/main/java/com/weseethemusic/music/service/MusicVisualizationServiceImpl.java
@@ -2,6 +2,12 @@ package com.weseethemusic.music.service;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nimbusds.jose.shaded.gson.Gson;
+import com.nimbusds.jose.shaded.gson.JsonArray;
+import com.nimbusds.jose.shaded.gson.JsonObject;
+import com.nimbusds.jose.shaded.gson.JsonParser;
+import com.weseethemusic.music.common.service.S3Service;
+import com.weseethemusic.music.dto.visualization.BackgroundDto;
 import com.weseethemusic.music.dto.visualization.LyricDto;
 import com.weseethemusic.music.dto.visualization.LyricListDto;
 import com.weseethemusic.music.dto.visualization.MusicVisualizationDto;
@@ -11,48 +17,65 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class MusicVisualizationServiceImpl implements MusicVisualizationService {
+
+    private final S3Service s3Service;
 
     // 음악 시각화 데이터 불러오기
     @Override
     public MusicVisualizationDto getMusicVisualization(long musicId) {
-        ObjectMapper mapper = new ObjectMapper();
         MusicVisualizationDto result = new MusicVisualizationDto();
 
-        try {
-            InputStream inputStream = getClass().getClassLoader().getResourceAsStream("data.json");
-
-            if (inputStream == null) {
-                throw new IllegalArgumentException("File not found: data.json");
-            }
-
-            JsonNode root = mapper.readTree(inputStream);
-            JsonNode dataNode = root.path("data");
-
-            List<TimeDurationDto> vibrations = new ArrayList<>();
-
-            for (JsonNode vibrationNode : dataNode.path("vibrations")) {
-                TimeDurationDto timeDurationDto = mapper.treeToValue(vibrationNode,
-                    TimeDurationDto.class);
-                vibrations.add(timeDurationDto);
-            }
-
-            result.setVibrations(vibrations);
-
-            List<ShapeDto> notes = new ArrayList<>();
-
-            for (JsonNode noteNode : dataNode.path("notes")) {
-                ShapeDto shapeDto = mapper.treeToValue(noteNode, ShapeDto.class);
-                notes.add(shapeDto);
-            }
-
-            result.setNotes(notes);
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (musicId != 713 && musicId != 2510 && musicId != 145) {
+            musicId = 713;
         }
+
+        String jsonString = s3Service.readJsonFile(musicId + ".json");
+
+        Gson gson = new Gson();
+        JsonObject root = JsonParser.parseString(jsonString).getAsJsonObject();
+        JsonObject dataNode = root.getAsJsonObject("data");
+
+        List<BackgroundDto> backgrounds = new ArrayList<>();
+        JsonArray backgroundsArray = dataNode.getAsJsonArray("backgrounds");
+
+        for (int i = 0; i < backgroundsArray.size(); i++) {
+            BackgroundDto backgroundDto = gson.fromJson(backgroundsArray.get(i),
+                BackgroundDto.class);
+
+            backgrounds.add(backgroundDto);
+        }
+
+        result.setBackgrounds(backgrounds);
+
+        List<TimeDurationDto> vibrations = new ArrayList<>();
+        JsonArray vibrationsArray = dataNode.getAsJsonArray("vibrations");
+
+        for (int i = 0; i < vibrationsArray.size(); i++) {
+            TimeDurationDto timeDurationDto = gson.fromJson(vibrationsArray.get(i),
+                TimeDurationDto.class);
+
+            vibrations.add(timeDurationDto);
+        }
+
+        result.setVibrations(vibrations);
+
+        List<ShapeDto> notes = new ArrayList<>();
+        JsonArray notesArray = dataNode.getAsJsonArray("notes");
+
+        for (int i = 0; i < notesArray.size(); i++) {
+            ShapeDto shapeDto = gson.fromJson(notesArray.get(i), ShapeDto.class);
+            notes.add(shapeDto);
+        }
+
+        result.setNotes(notes);
 
         return result;
     }
@@ -87,4 +110,5 @@ public class MusicVisualizationServiceImpl implements MusicVisualizationService 
 
         return result;
     }
+
 }
